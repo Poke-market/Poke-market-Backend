b6ec63b605e75935b7efe6614bc2af09f3ab6617
Caellumn <Vincent.stoop35@gmail.com>
2025-02-22 21:07:19 +0100

    created controllers and routes for items

--- src/routes/itemsRoutes.ts
+++ src/routes/itemsRoutes.ts
@@ -1,9 +1,28 @@
 import express from "express";
 
-import { getItems } from "../controllers/itemsController";
+import {
+  addItem,
+  deleteItem,
+  getItemById,
+  getItemByName,
+  getItems,
+  getItemsByCategory,
+  getItemsByCategoryQuerry,
+  getItemsByNameQuerry,
+  updateItem,
+} from "../controllers/itemsController";
 
 const router = express.Router();
 
-router.get("/", getItems);
+router
+  .post("/", addItem)
+  .delete("/:id", deleteItem)
+  .get("/", getItems)
+  .get("/name", getItemByName)
+  .patch("/:id", updateItem)
+  .get("/:id", getItemById)
+  .post("/category", getItemsByCategory) //.get werkt niet
+  .get("/category/:category", getItemsByCategoryQuerry)
+  .get("/name/:name", getItemsByNameQuerry);
 
 export default router;
