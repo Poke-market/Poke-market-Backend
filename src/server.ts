bc07cf2410734aededcf6bf3f82eede73f91f2ea
Joppe-VH <joppe.vanhoof@gmail.com>
2025-03-02 21:15:46 +0100

    use config file in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 // Imports
-import "dotenv/config";
+import { PORT, MONGO_URI } from "./config/env";
 import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
@@ -13,7 +13,6 @@ import authRoutes from "./routes/authRoutes";
 
 // Variables
 const app = express();
-const PORT = process.env.PORT ?? 3000;
 
 // Middleware
 app.use(cors());
@@ -30,12 +29,8 @@ app.all("*splat", notFound);
 app.use(errorHandler);
 
 // Database connection
-if (!process.env.MONGO_URI) {
-  throw new Error("Missing MONGO_URI environment variable in .env file");
-}
-await mongoose.connect(process.env.MONGO_URI);
 try {
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(MONGO_URI);
   console.log("Database connection OK");
 } catch (err) {
   console.error(err);
