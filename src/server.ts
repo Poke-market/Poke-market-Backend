2db9f8357910c3c5035209c0ac66e70f282f4528
Joppe-VH <joppe.vanhoof@gmail.com>
2025-03-08 04:50:57 +0100

    add handlebars

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { PORT, MONGO_URI } from "./config/env";
 import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
+import hbs from "./config/handlebars";
 
 import { notFound } from "./controllers/notFoundController";
 import { errorHandler } from "./middleware/errorMiddleware";
@@ -10,6 +11,7 @@ import itemsRoutes from "./routes/itemsRoutes";
 import tagRoutes from "./routes/tagRoutes";
 import userRoutes from "./routes/userRoutes";
 import authRoutes from "./routes/authRoutes";
+import webRoutes from "./routes/webRoutes";
 
 // Variables
 const app = express();
@@ -17,12 +19,23 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Routes
+// Configure Handlebars view engine
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("views", "src/views");
+app.use(express.static("src/public"));
+
+// API Routes
 app.use("/api/items", itemsRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/tags", tagRoutes);
 app.use("/api/auth", authRoutes);
+
+// Web Routes
+app.use("/", webRoutes);
+
 app.all("*splat", notFound);
 
 // handle errors (this must be last)
