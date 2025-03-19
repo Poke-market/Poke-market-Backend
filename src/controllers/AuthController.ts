987a5cf0e23e3e6f97623c74831c37cf564aa499
Caellumn <Vincent.stoop35@gmail.com>
2025-03-19 16:45:32 +0100

    mail verification

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -2,9 +2,10 @@ import { Request, Response } from "express";
 import { User } from "../models/userModel";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { IS_PRODUCTION, JWT_SECRET } from "../config/env";
+import { BASE_URL, IS_PRODUCTION, JWT_SECRET } from "../config/env";
 import { UnauthorizedError, ConflictError } from "../errors";
 import { z } from "zod";
+import { sendVerificationEmail } from "../utils/sendVerificationMail";
 
 function loginUser(userDocument: InstanceType<typeof User>, res: Response) {
   // strip password from returned user.
@@ -74,9 +75,23 @@ export const register = async (req: Request, res: Response) => {
   if (existingUser) throw new ConflictError("User already exists");
 
   const hashedPassword = await bcrypt.hash(password, 10);
+  const verificationToken = jwt.sign({ email }, JWT_SECRET, {
+    expiresIn: "1h",
+  });
+
+  const verificationLink = `${BASE_URL}/api/auth/verify?token=${verificationToken}`;
+
+  await sendVerificationEmail({
+    name: userData.firstname,
+    email: userData.email,
+    type: "verify",
+    link: verificationLink,
+  });
+
   const newUser = await User.create({
     ...userData,
     password: hashedPassword,
+    verificationToken,
   });
 
   const loggedInUser = loginUser(newUser, res);
@@ -113,4 +128,47 @@ export const logout = (req: Request, res: Response) => {
     sameSite: "lax",
   });
   res.status(200).json({ status: "success", data: null });
+};
+
+export const verficationEmail = async (req: Request, res: Response) => {
+  try {
+    // http://localhost:3000/verify/token
+    const { token } = req.params;
+
+    if (!token) {
+      res.status(400).json({ message: "Token is required" });
+      return;
+    }
+
+    // Verify the token
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    // Find the user by email
+    const user = await User.findOne({
+      email: (decoded as { email: string }).email,
+    });
+
+    if (user?.isVerified) {
+      res.status(400).json({ message: "Email already verified" });
+      return;
+    }
+
+    // If user is not found, return 404 error
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+
+    user.isVerified = true;
+    user.verificationToken = null;
+    await user.save();
+
+    res.status(200).json({ message: "you are a verified Pokémon trainer" });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
 };
