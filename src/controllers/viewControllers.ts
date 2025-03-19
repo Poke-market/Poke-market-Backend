98ee3d8d71663302d7eaf6ce191703bb6dc8b0ab
Caellumn <Vincent.stoop35@gmail.com>
2025-03-19 01:36:13 +0100

    register+loginpage

--- src/controllers/viewControllers.ts
+++ src/controllers/viewControllers.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { getItems } from "../services/itemService";
 import { makePageLinkBuilder } from "../utils/pageLinkBuilder";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config/env";
 
 export const renderItemsView = async (req: Request, res: Response) => {
   const { items, info } = await getItems(req.query, makePageLinkBuilder(req));
@@ -12,4 +14,46 @@ export const renderItemsView = async (req: Request, res: Response) => {
     sort: req.query.sort,
     order: req.query.order,
   });
+};
+
+export const renderLoginView = (req: Request, res: Response) => {
+  const tokenFromCookie: string | undefined =
+    typeof req.cookies?.token === "string" ? req.cookies.token : undefined;
+  const tokenFromHeader: string | undefined =
+    req.headers.authorization?.startsWith("Bearer")
+      ? req.headers.authorization.split(" ")[1]
+      : undefined;
+  const token: string | undefined = tokenFromCookie ?? tokenFromHeader;
+
+  if (token) {
+    jwt.verify(token, JWT_SECRET);
+    return res.redirect("/items");
+  }
+
+  // If no token or invalid token, return to login page
+  res.render("login", {
+    title: "Poke-Mart Login",
+  });
+};
+
+export const renderRegisterView = (req: Request, res: Response) => {
+  const tokenFromCookie: string | undefined =
+    typeof req.cookies?.token === "string" ? req.cookies.token : undefined;
+  const tokenFromHeader: string | undefined =
+    req.headers.authorization?.startsWith("Bearer")
+      ? req.headers.authorization.split(" ")[1]
+      : undefined;
+  const token: string | undefined = tokenFromCookie ?? tokenFromHeader;
+
+  //if token available render register page
+  if (token) {
+    res.render("register", {
+      title: "Poke-Mart Register",
+    });
+  }
+
+  // If no token or invalid token, return to register page
+  if (!token) {
+    res.redirect("/login");
+  }
 };
