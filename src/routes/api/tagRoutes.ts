1b9f33ba9ad7b834dacd3b74d46442f9eead909e
Joppe-VH <joppe.vanhoof@gmail.com>
2025-03-23 20:44:00 +0100

    proper permission check for tag and user routes

--- src/routes/api/tagRoutes.ts
+++ src/routes/api/tagRoutes.ts
@@ -8,6 +8,7 @@ import {
   getTags,
   updateTag,
 } from "../../controllers/api/tagController";
+import { is } from "../../middleware/isMiddleware";
 
 /**
  * @openapi
@@ -20,9 +21,9 @@ const router = express.Router();
 
 router
   .get("/", getTags)
-  .post("/", addTag)
-  .delete("/:id", deleteTag)
-  .patch("/:id", updateTag)
+  .post("/", is("admin"), addTag)
+  .delete("/:id", is("admin"), deleteTag)
+  .patch("/:id", is("admin"), updateTag)
   .get("/:id", getTagById)
   .get("/name/:name", getTagByName);
 
