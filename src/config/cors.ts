039184407c34761c6b61b7a9f7b32bb39e9e8d97
Joppe-VH <joppe.vanhoof@gmail.com>
2025-03-25 08:21:54 +0100

    add fork link

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -6,6 +6,7 @@ export const corsOptions = {
         "https://poke-markt-webshop.vercel.app",
         "https://strong-ward-453503-f1.web.app/",
         "https://strong-ward-453503-f1.firebaseapp.com/",
+        "https://poke-markt-webshop-fork.vercel.app",
       ]
     : true,
   credentials: true,
